b7a358ecfdd00921984f99b6163ed0acded83bfe
Carissa <carissablossom@gmail.com>
2016-06-15 16:12:17 -0700

    create book list and basic filler for book detail

---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,11 @@
+//reducers are only called when an action occurred
+//state argument is not application state,
+//only the state that this reducer is responsible for
+export default function(state = null, action) {
+  switch(action.type) {
+    case 'BOOK_SELECTED':
+      return action.payload;
+  }
+  
+  return state;
+}
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+// returns an array of books
+export default function() {
+  return [
+    {title: 'Javscript: The Good Parts'},
+    {title: 'Harry Potter & The Prizoner of Azkaban'},
+    {title: 'Eloquent Java'},
+    {title: 'The Historian'}
+  ]
+}
